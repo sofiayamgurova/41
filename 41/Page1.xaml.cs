d995f2ea90db4d174c09efe0b19ebf3dd597a6c2
Vais Sofia <sofiavajs626@gmail.com>
2025-03-03 21:47:51 +0500

    Добавьте файлы проекта.

---
+++ 41/Page1.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _41
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void LogingGuest_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTB.Text;
+            string password = PassTB.Text;
+            User user = Yamgurova41Entities.GetContext().User.ToList().Find(p => p.UserLogin == login && p.UserPassword == password);
+            Manager.MainFrame.Navigate(new ProductPage(user));
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTB.Text;
+            string password = PassTB.Text;
+            if (login == "" || password == "")
+            {
+                MessageBox.Show("Есть пустые поля");
+                return;
+            }
+
+            User user = Yamgurova41Entities.GetContext().User.ToList().Find(p => p.UserLogin == login && p.UserPassword == password);
+            if (user != null)
+            {
+                Manager.MainFrame.Navigate(new ProductPage(user));
+                LoginTB.Text = "";
+                PassTB.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Введены неверные данные");
+                LoginButton.IsEnabled = false;
+                Thread.Sleep(10000); // 10 секунд таймер
+                LoginButton.IsEnabled = true;
+            }
+        }
+    }
+}
---
+++ 41/ProductPage.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _41
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductPage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+        public ProductPage(User user)
+        {
+            InitializeComponent();
+
+            if (user == null)
+            {
+                FIOTB.Text = "";
+                RoleTB.Text = "";
+            }
+            else
+            {
+                FIOTB.Text = user.UserSurname + " " + user.UserName + " " + user.UserPatronymic;
+                switch (user.UserRole)
+                {
+                    case 1:
+                        RoleTB.Text = "Клиент"; break;
+                    case 2:
+                        RoleTB.Text = "Менеджер"; break;
+                    case 3:
+                        RoleTB.Text = "Администратор"; break;
+                }
+            }
+
+            var currentProduct = Yamgurova41Entities.GetContext().Product.ToList();
+
+            ProductListView.ItemsSource = currentProduct;
+
+            ComboType.SelectedIndex = 0;
+
+            UpdateProduct();
+        }
+        public void UpdateProduct()
+        {
+            var currentProducts = Yamgurova41Entities.GetContext().Product.ToList();
+
+            if (ComboType.SelectedIndex == 0)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) <= 100)).ToList();
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) < 10)).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 10 && Convert.ToInt32(p.ProductDiscountAmount) < 15)).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 15 && Convert.ToInt32(p.ProductDiscountAmount) < 100)).ToList();
+            }
+
+            if (RButtonDown.IsChecked.Value)
+            {
+                currentProducts = currentProducts.OrderByDescending(p => p.ProductCostInt).ToList();
+            }
+            if (RButtonUp.IsChecked.Value)
+            {
+                currentProducts = currentProducts.OrderBy(p => p.ProductCostInt).ToList();
+            }
+
+            currentProducts = currentProducts.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            TBlockCount.Text = Convert.ToString(currentProducts.Count());
+            TBlockMaxCount.Text = Convert.ToString(Yamgurova41Entities.GetContext().Product.Count());
+
+            ProductListView.ItemsSource = currentProducts.ToList();
+        }
+
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateProduct();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateProduct();
+        }
+    }
+}
